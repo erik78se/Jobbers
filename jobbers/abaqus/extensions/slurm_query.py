e552e9ebfb8f7dd1c158bc46bcf410d4833b0a05
sssnks <Frus3tV4tt3n!>
2019-05-24 11:00:56 +0200

    Code cleanup

--- jobbers/abaqus/extensions/slurm_query.py
+++ jobbers/abaqus/extensions/slurm_query.py
@@ -3,10 +3,9 @@ import subprocess
 
 def query_jobs_by_user(user_name):
 
+    # Query SLURM for current jobs by user
     command = ['squeue', '-o', '"%A;%t;%j"', '-h', '-u', user_name]
-
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
     try:
         (std_out, std_err) = process.communicate(timeout=6)
     except subprocess.TimeoutExpired:
@@ -15,24 +14,25 @@ def query_jobs_by_user(user_name):
 
     # Convert byte string to string
     current_jobs = std_out.decode(errors='replace').strip().split("\n")
-    #current_jobs = std_out.decode(errors='replace')
 
     job_array = list()
     if current_jobs != ['']:
         for job in current_jobs:
-            job_id, state, job_name = job.split(';')
-            job_array.append([job_id, state, job_name])
-
+            job_id, job_state, job_name = job.split(';')
+            job_array.append([job_id, job_state, job_name])
         return job_array
-    else
+    else:
         return False
 
 
 if __name__ == '__main__':
+    """
+    Test code for simple debugging of modules
+    """
     from getpass import getuser
 
     print(f' Current user name "{getuser()}" ')
     jobs = query_jobs_by_user(getuser())
-    for job in jobs:
-        id, state, name = job
-        print(f' {id} - {state} - {name}')
+    for a_job in jobs:
+        id_job, state, name = a_job
+        print(f' {id_job} - {state} - {name}')
